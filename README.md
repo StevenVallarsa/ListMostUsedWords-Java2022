8a845cce0f27e67b61dc9839c7e40a81b0bc4164
StevenVallarsa <orionsjv@gmail.com>
2022-01-08 13:19:02 -0500

    completed implementation

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 A program that will:
 - break a paragraph of text into an array of Strings
 - iterate through the array and put each word into a HashMap
-- - if word is not already in HashMap, insert it as a key with a value of 1 using PUT
-- - if word does exist, add 1 to the value of that key (by using PUT again to overwrite the existing item)
+   - if word is not already in HashMap, insert it as a key with a value of 1 using PUT
+   - if word does exist, add 1 to the value of that key (by using PUT again to overwrite the existing item)
 - order HashMap on values
+  - was originally going to try and sort the HashMap by value, but the syntax is way beyond my current understanding, so I will use the brute force method instead where I will iterate through the HashMap and find the (first) highest value, then put that into a new HashMap and delete it from the original hashmap.
+  - Condiute doing this until I have a minimum of 5 items, but chceck to make sure I pick up all the items at the lowest value of these (max) 5 items. 
 - display words with the top five occurances (there could be more than five words due to ties)
 
 ###  Tech Stack
--- src/main/java/com/sv/listmostusedwordsinparagraph2022/ListMostUsedWords.java
+++ src/main/java/com/sv/listmostusedwordsinparagraph2022/ListMostUsedWords.java
@@ -5,14 +5,8 @@
 
 package com.sv.listmostusedwordsinparagraph2022;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -26,7 +20,9 @@ import java.util.Set;
 public class ListMostUsedWords {
     public static void main(String[] args) {
         
-        String paragraph = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.";
+//        String paragraph = "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.";
+
+        String paragraph = "In the late summer of that year we lived in a house in a village that looked across the river and the plain to the mountains. In the bed of the river there were pebbles and boulders, dry and white in the sun, and the water was clear and swiftly moving and blue in the channels. Troops went by the house and down the road and the dust they raised powdered the leaves of the trees. The trunks of the trees too were dusty and the leaves fell early that year and we saw the troops marching along the road and the dust rising and leaves, stirred by the breeze, falling and the soldiers marching and afterward the road bare and white except for the leaves.";
         
         Map<String, Integer> wordCountHashMap = new HashMap<>();
         
@@ -51,25 +47,50 @@ public class ListMostUsedWords {
             }
         }
         // print unsorted HashMap
-        System.out.println("\nUnsorted HashMap");
-        Set<String> keys = wordCountHashMap.keySet();
-
-        for(String key : keys) {
-            System.out.println(key + ": " + wordCountHashMap.get(key));
-        }
+//        System.out.println("\nUnsorted HashMap");
+//        Set<String> keys = wordCountHashMap.keySet();
+//
+//        for(String key : keys) {
+//            System.out.println(key + ": " + wordCountHashMap.get(key));
+//        }
         
         /**
          * Sort HashMap by value
          * - create list of entries
          * - sort the list
-         * - put data from sorted list into new HashMap
+         * - put data from sorted list into new LinkedHashMap (to preserve order)
          */
+        Map<String, Integer> topWordsByCount = new LinkedHashMap<>();
         
-        // display top five words
+        boolean isStillIterating = true;
+        int lastTopWordCount = 0;
         
-//        System.out.println(Arrays.toString(wordsArray));
-        
+        while (isStillIterating) {
+            String topWord = "";
+            int topWordCount = 0;
 
+            for (String key : wordCountHashMap.keySet()) {
+                if (wordCountHashMap.get(key) > topWordCount) {
+                    topWordCount = wordCountHashMap.get(key);
+                    topWord = key;
+                }
+            }
+            
+            // keep adding words that have the same wordcount as the one in the #5 position
+            if (topWordsByCount.size() < 5 || topWordCount == lastTopWordCount) {
+                topWordsByCount.put(topWord, topWordCount);
+                wordCountHashMap.remove(topWord);
+                lastTopWordCount = topWordCount;
+            } else {
+                isStillIterating = false;
+            }
+        }
         
+        // Print top words by count
+        System.out.println("\nTop " + topWordsByCount.size() + " Words by Count");
+        for(String key : topWordsByCount.keySet()) {
+            System.out.println(key + ": " + topWordsByCount.get(key));
+        }
+//        System.out.println(Arrays.toString(wordsArray));
     }
 }
