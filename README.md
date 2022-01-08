cc74b47a2aa4fe4a71020fdd4597422896c18c26
StevenVallarsa <orionsjv@gmail.com>
2022-01-08 09:54:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# List Most Used Words In Paragraph
+A program that will:
+- break a paragraph of text into an array of Strings
+- iterate through the array and put each word into a HashMap
+- - if word is not already in HashMap, insert it as a key with a value of 1 using PUT
+- - if word does exist, add 1 to the value of that key (by using PUT again to overwrite the existing item)
+- order HashMap on values
+- display words with the top five occurances (there could be more than five words due to ties)
+
+###  Tech Stack
+- Java 11
+- NetBeans 12.5
---
+++ src/main/java/com/sv/listmostusedwordsinparagraph2022/ListMostUsedWords.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.sv.listmostusedwordsinparagraph2022;
+
+/**
+ *
+ * @author: Steven Vallarsa
+ *   email: stevenvallarsa@gmail.com
+ *    date: 2022-01-08
+ * purpose: A program that will list the 5 most used words in a paragraph of text
+ */
+public class ListMostUsedWords {
+    public static void main(String[] args) {
+        
+    }
+}
